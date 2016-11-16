4703300f8b1fba86d80eb30280d6d6dc256ac859
혜진 <@SHJ>
2016-11-16 15:15:28 +0900

    커밋

--- src/charge/controller/ChargeController.java
+++ src/charge/controller/ChargeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import charge.model.ChargeService;
+import charge.model.ChargeUseService;
 
 @Controller
 @RequestMapping("/charge")
@@ -18,17 +19,24 @@ public class ChargeController {
 
 	@Autowired
 	ChargeService charge;
+	@Autowired
+	ChargeUseService use;
 	
 	// 충전하기
 	@RequestMapping("/cash")
 	@ResponseBody
-	public String charge(HttpSession id, int point){
-		boolean b = charge.charge((String)id.getAttribute("id"), point);
-		if(b){
-			return "true";
-		} else{
-			return "false";
+	public boolean charge(HttpSession id, String take, String point){
+		int p = Integer.parseInt(point.substring(0, point.lastIndexOf("원")));
+		int a = 0;
+		if(take.equals("나에게")){
+			a = charge.charge((String)id.getAttribute("id"), p);
+		}else{
+			a = use.gift((String)id.getAttribute("id"), take, p);
 		}
+		if(a==1){
+			return true;
+		}
+		return false;
 	}
 	
 	// 충전내역
