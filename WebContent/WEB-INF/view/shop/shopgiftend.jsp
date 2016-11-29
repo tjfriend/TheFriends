5f1d9b5c6dbcf32258124fcb713e75f5dfff48e7
User <User@minjung-0318>
2016-11-29 21:24:49 +0900

    야호!!!

---
+++ WebContent/WEB-INF/view/shop/shopgiftend.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<c:choose>
+	<c:when test="${m==1 }">
+		<p>선물에 성공하였습니다.</p> <br/>
+		<input type="button" value="확인" onclick="self.close()" />
+	</c:when>
+	<c:otherwise>
+		<p>선물에 실패하였습니다.</p> 
+		<p>보유잣이 부족하거나 친구가 같은 음악이 있습니다.</p> <br/>
+		<input type="button" value="닫기" onclick="self.close()" />
+		
+	</c:otherwise>
+</c:choose>
--- src/shop/model/Shopgift.java
+++ src/shop/model/Shopgift.java
@@ -1,5 +1,7 @@
 package shop.model;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
@@ -11,9 +13,50 @@ import org.springframework.stereotype.Component;
 public class Shopgift {
 	@Autowired
 	SqlSessionFactory fac;
-	
-	public List friend(String id){
+
+	public List friend(String id) {
 		SqlSession ss = fac.openSession();
 		return ss.selectList("friends.friendSearch", id);
 	}
+
+	public int friendgift(String id, String title, String money, String gtake) {
+		HashMap<String, Object> map = new HashMap<>();
+		 map.put("id", gtake);
+		 map.put("title", title);
+		SqlSession sql = fac.openSession();
+		try {
+			List list = sql.selectList("shopbuy.musiccheck", map);
+			List<HashMap> list2 = sql.selectList("shopbuy.musicmoney", map);
+			List<HashMap> list3 = sql.selectList("shopbuy.mypoint", map);
+			int musicmoney = ((BigDecimal) list2.get(0).get("MONEY")).intValue();
+			int mypoint = ((BigDecimal) list3.get(0).get("POINT")).intValue();
+			if (list.size() == 0) {
+				if (musicmoney <= mypoint) {
+					HashMap<String, Object> pmap = new HashMap<>();
+					pmap.put("id", id);
+					pmap.put("money", musicmoney);
+					sql.update("shopbuy.cash", pmap);
+					sql.insert("shopbuy.musicbuy", map);
+					sql.commit();
+					sql.close();
+					return 1;
+				} else {
+					sql.rollback();
+					sql.close();
+					return -1;
+				}
+			} else {
+				sql.rollback();
+				sql.close();
+				return -1;
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+			sql.rollback();
+			sql.close();
+			return -1;
+
+		}
+	}
+
 }
